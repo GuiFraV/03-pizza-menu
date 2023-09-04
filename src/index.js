c164bc1952909d27e880287042de89952aaeb15e
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-04 18:56:22 +0200

    add some shortcircuit condition

--- src/index.js
+++ src/index.js
@@ -72,16 +72,21 @@ function Header() {
 )}
 
 function Menu() {
+
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className='menu'>
             <h2>Our menu</h2>
-
-            <ul className='pizzas'>
-                {pizzaData.map((pizza) => (
-                    <Pizza pizzaObj={pizza} key={pizza.name}/>
-                
-                ))}
-            </ul>
+            {numPizzas > 0 &&(     
+                <ul className='pizzas'>
+                    {pizzas.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name}/>
+                    
+                    ))}
+                </ul>
+            )}
 
             {/* <Pizza 
                 name='Pizza Spinaci' 
@@ -131,8 +136,14 @@ function Footer() {
 
     return (
         <footer className='footer'>
-            {new Date().toLocaleTimeString()}
-            . We're currently open !
+            <div className='order'>
+                {isOpen && (
+                    <p>
+                        We're open until {closeHour}:00. Come visit us or order online.
+                    </p>
+                )}
+                <button className='btn'>Order</button>
+            </div>
         </footer>
     // return React.createElement('footer', null, "We're currently open!")
 )}
