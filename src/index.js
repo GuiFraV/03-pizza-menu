ce262a38db87a7865fa88703bf17e123837dbfdc
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-05 08:45:38 +0200

    add some react fragment

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function App() {
     return (
         <div className='container'>
             <Header />  
-                <Menu />
+            <Menu />
             <Footer />
         </div>
     );
@@ -81,24 +81,33 @@ function Menu() {
         <main className='menu'>
             <h2>Our menu</h2>
 
-            {numPizzas > 0  ? (     
-                <ul className='pizzas'>
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name}/>
-                    
-                    ))}
-                </ul>
-            ) : <p>We're still working on our menu. Please come back later :) </p>}
+
+            {numPizzas > 0  ? (    
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All
+                        from our stone oven, all organic, all delicious.
+                    </p>
+                    <ul className='pizzas'>
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name}/>
+                            
+                            ))}
+                    </ul>
+                </>   
+            ) : <p>We're still working on our menu. Please come back later :) </p>
+            
+            }
 
             {/* <Pizza 
                 name='Pizza Spinaci' 
                 ingredient='Tomato, mozarella, spinach, and ricotta cheese ' 
                 photoName='pizzas/spinaci.jpg' 
                 price={10}
-            />
-
-
-            <Pizza 
+                />
+                
+                
+                <Pizza 
                 name='Pizza Funghi' 
                 ingredient='Tomato, mozarella, mushrooms, and onion'
                 price={12}
